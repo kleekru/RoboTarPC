334911f0c1b67cf057a5d943822d11a3fb95cd67
Miroslav Mocek <miira@versarius.cz>
2014-01-19 14:14:54 +0100

    docs and supporting methods for traversal of chords in edit mode

--- src/main/java/ioio/robotar/pcconsole/PositionHint.java
+++ src/main/java/ioio/robotar/pcconsole/PositionHint.java
@@ -3,9 +3,13 @@ package ioio.robotar.pcconsole;
 import cz.versarius.xsong.ChordRef;
 
 public class PositionHint {
+	/** absolute position in document */
 	private int offset;
+	/** length of the line, aka text part length. (currently the same as chord part length) */
 	private int length;
+	/** index of line. chords and lines share the same line index. starts with 0. */
 	private int line;
+	/** reference to chord */
 	private ChordRef chordRef;
 	
 	public int getLength() {
--- src/main/java/ioio/robotar/pcconsole/PositionHints.java
+++ src/main/java/ioio/robotar/pcconsole/PositionHints.java
@@ -6,8 +6,12 @@ import java.util.List;
 public class PositionHints {
 	private List<PositionHint> chords = new ArrayList<PositionHint>();
 	private List<PositionHint> lines = new ArrayList<PositionHint>();
+	/** used for playing song */
 	private int currentChord;
+	/** used for playing song */
 	private int currentLine;
+	/** used for editing */
+	private PositionHint lastSelectedChord;
 	
 	public List<PositionHint> getChords() {
 		return chords;
@@ -47,6 +51,28 @@ public class PositionHints {
 		}
 		return chords.get(currentChord);
 	}
+	
+	public PositionHint getNextChordHint(PositionHint chordHint) {
+		int curr = chords.indexOf(chordHint);
+		if (curr < 0 ) {
+			return null;
+		}
+		curr++;
+		if (curr == chords.size()) {
+			return null;
+		}
+		return chords.get(curr);
+	}
+	
+	public PositionHint getPrevChordHint(PositionHint chordHint) {
+		int curr = chords.indexOf(chordHint);
+		curr--;
+		if (curr < 0) {
+			return null;
+		}
+		return chords.get(curr);
+	}
+	
 	public PositionHint getLineHint(PositionHint chordHint) {
 		if (chordHint == null) {
 			return null;
@@ -58,11 +84,78 @@ public class PositionHints {
 		}
 		return lineHint;
 	}
+	public int getLineOffset(PositionHint chordHint) {
+		return lines.get(chordHint.getLine()).getOffset();
+	}
+	public int getPrevLineOffset(PositionHint chordHint, int min) {
+		int lineidx = chordHint.getLine();
+		if (lineidx == 0) {
+			return min;
+		}
+		return lines.get(--lineidx).getOffset();
+	}
+	public int getNextLineOffset(PositionHint chordHint, int max) {
+		int lineidx = chordHint.getLine();
+		if (lineidx >= (lines.size() - 1)) {
+			return max;
+		}
+		return lines.get(++lineidx).getOffset();
+	}
+	
 	public int lookAhead(PositionHint lineHint) {
 		int ahead = currentLine + 2;
 		if (lines.size() <= ahead) {
 			return -1;
 		}
 		return lines.get(ahead).getOffset();
+	}
+	public PositionHint getLastSelectedChord() {
+		return lastSelectedChord;
+	}
+	public void setLastSelectedChord(PositionHint lastSelectedChord) {
+		this.lastSelectedChord = lastSelectedChord;
+	}
+	/** 
+	 * Finds the nearest chord before specified position in doc.
+	 * @param position
+	 * @return null if no such chord
+	 */
+	public PositionHint findChordBefore(int position) {
+		if (chords.isEmpty()) {
+			return null;
+		}
+		int next = 0;
+		PositionHint curr = chords.get(next);
+		PositionHint before = null;
+		while (curr.getOffset() < position && next < chords.size() - 1) {
+			next++;
+			before = curr;
+			curr = chords.get(next);
+		}
+		if (curr.getOffset() < position) {
+			return curr;
+		} else {
+			return before;
+		}
+	}
+	
+	/** 
+	 * Finds the nearest chord after specified position in doc.
+	 */
+	public PositionHint findChordAfter(int position) {
+		if (chords.isEmpty()) {
+			return null;
+		}
+		int next = 0;
+		PositionHint curr = chords.get(next);
+		while (curr.getOffset() < position && next < chords.size() - 1) {
+			next++;
+			curr = chords.get(next);
+		}
+		if (curr.getOffset() > position) {
+			return curr;
+		} else {
+			return null;
+		}
 	}
 }
