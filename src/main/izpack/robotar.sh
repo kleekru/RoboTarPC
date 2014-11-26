272d91519e865627ea5acb4c46cc6320a2d2db66
Miroslav Mocek <miira@versarius.cz>
2014-11-26 22:14:28 +0100

    display COM port selection only on Windows

--- src/main/izpack/robotar.sh
+++ src/main/izpack/robotar.sh
@@ -1,6 +1,5 @@
-echo "Port selected: "${PORT_SELECTED}
 echo "app version  : "${APP_VER}
 
-java -Dioio.SerialPorts=COM${PORT_SELECTED} -jar ./RoboTarPC-${APP_VER}.jar
+java -jar ./RoboTarPC-${APP_VER}.jar
 
  
